5794a6d36ef6fb841308d173c1b06c3cb61230a5
steven-kid <stevenkid404@gmail.com>
2025-01-01 00:42:12 +0800

    feat: update

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       .toString()
       .padStart(2, "0");
 
-    const result = `第 <span class="digit">${days}</span> 天 <span class="digit">${hours}</span> 小时 <span class="digit">${minutes}</span> 分钟 <span class="digit">${seconds}</span> 秒`;
+    const result = ` <span class="digit">${days}</span> days <span class="digit">${hours}</span> hours <span class="digit">${minutes}</span> mins <span class="digit">${seconds}</span> seconds`;
     document.getElementById("clock").innerHTML = result;
   };
 
